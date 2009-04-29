68c5ed104d4dc9571dc9bb56fd0ccf5aea8af7b7
Michael Granger <ged@FaerieMUD.org>
2009-04-29 16:59:06 +0000

    * Added more RFC2252 regexp patterns to constants.rb.  * Upgraded spec helpers to use the webkit RSpec formatter if it's available.
    --HG--
    extra : convert_revision : svn%3Aa6044dcd-02c3-dd11-8301-000e0c4bc663/trunk%4021

--- lib/treequel/constants.rb
+++ lib/treequel/constants.rb
@@ -28,28 +28,28 @@ module Treequel::Constants # :nodoc:
 		# Terminals
 		# 
 		begin
-			# attr-type-chars          = ALPHA / DIGIT / "-"
+			# attr-type-chars		   = ALPHA / DIGIT / "-"
 			attr_type_chars = %r{ [[:alpha:][:digit:]\-] }x
 
-			# ldap-oid                 = 1*DIGIT 0*1("." 1*DIGIT)
-			#                            ; An LDAPOID, as defined in [4]
-			LDAP_OID        = %r{ [[:digit:]]+ (?:\.[[:digit:]]+)* }x
+			# ldap-oid				   = 1*DIGIT 0*1("." 1*DIGIT)
+			#							 ; An LDAPOID, as defined in [4]
+			LDAP_OID		= %r{ [[:digit:]]+ (?:\.[[:digit:]]+)* }x
 
-			# AttributeType            = ldap-oid / (ALPHA *(attr-type-chars))
+			# AttributeType			   = ldap-oid / (ALPHA *(attr-type-chars))
 			LDAP_ATTRIBUTE_TYPE = %r{
 				#{LDAP_OID}
 				|
 				[[:alpha:]] #{attr_type_chars}*
 			}x
 			
-			# opt-char                 = attr-type-chars
+			# opt-char				   = attr-type-chars
 			opt_char = attr_type_chars
 			
-			# option                   = 1*opt-char
+			# option				   = 1*opt-char
 			option = %r{ #{opt_char}+ }x
 			
-            # 
-			# AttributeDescription     = AttributeType [";" options]
+			# 
+			# AttributeDescription	   = AttributeType [";" options]
 			LDAP_ATTRIBUTE_DESCRIPTION = %r{
 				(#{LDAP_ATTRIBUTE_TYPE})		# $1: attribute oid or name
 				((?:
@@ -60,13 +60,13 @@ module Treequel::Constants # :nodoc:
 			
 			# If a value should contain any of the following characters
 			# 
-			#            Character       ASCII value
-			#            ---------------------------
-			#            *               0x2a
-			#            (               0x28
-			#            )               0x29
-			#            \               0x5c
-			#            NUL             0x00
+			#			 Character		 ASCII value
+			#			 ---------------------------
+			#			 *				 0x2a
+			#			 (				 0x28
+			#			 )				 0x29
+			#			 \				 0x5c
+			#			 NUL			 0x00
 			# 
 			# the character must be encoded as the backslash '\' character (ASCII
 			# 0x5c) followed by the two hexadecimal digits representing the ASCII
@@ -75,22 +75,22 @@ module Treequel::Constants # :nodoc:
 			LDAP_ATTRIBUTE_VALUE = %r{[^\*\(\)\\\0]+|\\(?i:[0-9a-z]{2})}
 			
 			
-			# initial    = value
+			# initial	 = value
 			initial = LDAP_ATTRIBUTE_VALUE
 
-			# any        = "*" *(value "*")
+			# any		 = "*" *(value "*")
 			any = %r{
 				\*
 				(?:#{LDAP_ATTRIBUTE_VALUE}+\*)*
 			}x
 
-			# final      = value
+			# final		 = value
 			final = LDAP_ATTRIBUTE_VALUE
 
 			# The value part of a substring filter
 			LDAP_SUBSTRING_VALUE = %r{ #{initial}* #{any} #{final}* }x
 
-			# substring  = attr "=" [initial] any [final]
+			# substring	 = attr "=" [initial] any [final]
 			LDAP_SUBSTRING_FILTER = %r{
 				(#{LDAP_ATTRIBUTE_DESCRIPTION})
 				=
@@ -105,80 +105,124 @@ module Treequel::Constants # :nodoc:
 		# 
 		begin
 			
-			# a     = "a" / "b" / "c" / "d" / "e" / "f" / "g" / "h" / "i" /
-			#         "j" / "k" / "l" / "m" / "n" / "o" / "p" / "q" / "r" /
-			#         "s" / "t" / "u" / "v" / "w" / "x" / "y" / "z" / "A" /
-			#         "B" / "C" / "D" / "E" / "F" / "G" / "H" / "I" / "J" /
-			#         "K" / "L" / "M" / "N" / "O" / "P" / "Q" / "R" / "S" /
-			#         "T" / "U" / "V" / "W" / "X" / "Y" / "Z"
-            a = '[[:alpha:]]'
+			# a				  = "a" / "b" / "c" / "d" / "e" / "f" / "g" / "h" / "i" /
+			#					"j" / "k" / "l" / "m" / "n" / "o" / "p" / "q" / "r" /
+			#					"s" / "t" / "u" / "v" / "w" / "x" / "y" / "z" / "A" /
+			#					"B" / "C" / "D" / "E" / "F" / "G" / "H" / "I" / "J" /
+			#					"K" / "L" / "M" / "N" / "O" / "P" / "Q" / "R" / "S" /
+			#					"T" / "U" / "V" / "W" / "X" / "Y" / "Z"
+			a = '[[:alpha:]]'
 
-			# d               = "0" / "1" / "2" / "3" / "4" /
-			#                   "5" / "6" / "7" / "8" / "9"
+			# d				  = "0" / "1" / "2" / "3" / "4" /
+			#					"5" / "6" / "7" / "8" / "9"
 			d = '[[:digit:]]'
-            
-			# hex-digit       =  d / "a" / "b" / "c" / "d" / "e" / "f" /
-			#                        "A" / "B" / "C" / "D" / "E" / "F"
+			
+			# hex-digit		  =	 d / "a" / "b" / "c" / "d" / "e" / "f" /
+			#						 "A" / "B" / "C" / "D" / "E" / "F"
 			hexdigit = '[[:xdigit:]]'
 
-			# k               = a / d / "-" / ";"
+			# k				  = a / d / "-" / ";"
 			k = '[[:alpha:][:digit:];\-]'
 
-			# p               = a / d / """ / "(" / ")" / "+" / "," /
-			#                   "-" / "." / "/" / ":" / "?" / " "
+			# p				  = a / d / """ / "(" / ")" / "+" / "," /
+			#					"-" / "." / "/" / ":" / "?" / " "
 			p = '[[:alpha:][:digit:]"\(\)\+,\-\./:\? ]'
 
-			# anhstring       = 1*k
-            anhstring = /#{k}+/
+		 	# letterstring    = 1*a
+		 	letterstring = "#{a}+"
+		
+		 	# numericstring   = 1*d
+			numericstring = "#{d}+"
+		 	
+			# anhstring		  = 1*k
+			anhstring = /#{k}+/
 
-			# keystring       = a [ anhstring ]
-            keystring = /#{a}#{anhstring}/
+			# keystring		  = a [ anhstring ]
+			keystring = /#{a}#{anhstring}/
 
 			# printablestring = 1*p
-            #
-			# space           = 1*" "
-            # 
-			# whsp            = [ space ]
-            # 
-			# utf8            = <any sequence of octets formed from the UTF-8 [9]
-			#                    transformation of a character from ISO10646 [10]>
-            # 
-			# dstring         = 1*utf8
-            # 
-			# qdstring        = whsp "'" dstring "'" whsp
-            # 
-			# qdstringlist    = [ qdstring *( qdstring ) ]
-            # 
-			# qdstrings       = qdstring / ( whsp "(" qdstringlist ")" whsp )
+			printablestring = /#{p}+/
 
+			# space			  = 1*" "
+			# whsp			  = [ space ]
+			whsp = /[ ]*/
+
+			# descr           = keystring
+			descr = keystring
+			 
+			# numericoid      = numericstring *( "." numericstring )
+			numericoid = /#{numericstring} (?: \. #{numericstring} )*/x
+			
+			# oid             = descr / numericoid
+			oid = /#{descr}|#{numericoid}/
+
+			# woid            = whsp oid whsp
+			woid = /#{whsp}#{oid}#{whsp}/
+			
+			# oidlist         = woid *( "$" woid )
+			oidlist = /#{woid} (?:\$ #{woid})*/x
+			
+			# ; set of oids of either form
+			# oids            = woid / ( "(" oidlist ")" )
+			oids = /#{woid} | \( #{oidlist} \) /x
+			
+			# ; object descriptors used as schema element names
+			# qdescrs         = qdescr / ( whsp "(" qdescrlist ")" whsp )
+			qdescr	= %r{ #{whsp} ' #{descr} ' #{whsp} }x
+			
+			# qdescrlist      = [ qdescr *( qdescr ) ]
+			qdescrlist = /#{qdescr}+/x
+			
+			# qdescr		  = whsp "'" descr "'" whsp
+			qdescrs = /#{qdescr} | \( #{whsp} #{qdescr}+ #{whsp} \)/x
+
+			# utf8			  = <any sequence of octets formed from the UTF-8 [9]
+			#					 transformation of a character from ISO10646 [10]>
+			# 
+			# dstring		  = 1*utf8
+			# 
+			# qdstring		  = whsp "'" dstring "'" whsp
+			# 
+			# qdstringlist	  = [ qdstring *( qdstring ) ]
+			# 
+			# qdstrings		  = qdstring / ( whsp "(" qdstringlist ")" whsp )
+			qdstring = /#{whsp} ' (?:[^']+|\\')+ ' #{whsp} /x
+			qdstringlist = /#{qdstring}+/
+			qdstrings = /#{qdstring} | #{whsp} \( #{qdstringlist} \) #{whsp} /x
+			
 			# ObjectClassDescription = "(" whsp
-			#     numericoid whsp      ; ObjectClass identifier
-			#     [ "NAME" qdescrs ]
-			#     [ "DESC" qdstring ]
-			#     [ "OBSOLETE" whsp ]
-			#     [ "SUP" oids ]       ; Superior ObjectClasses
-			#     [ ( "ABSTRACT" / "STRUCTURAL" / "AUXILIARY" ) whsp ]
-			#                          ; default structural
-			#     [ "MUST" oids ]      ; AttributeTypes
-			#     [ "MAY" oids ]       ; AttributeTypes
+			#	  numericoid whsp	   ; ObjectClass identifier
+			#	  [ "NAME" qdescrs ]
+			#	  [ "DESC" qdstring ]
+			#	  [ "OBSOLETE" whsp ]
+			#	  [ "SUP" oids ]	   ; Superior ObjectClasses
+			#	  [ ( "ABSTRACT" / "STRUCTURAL" / "AUXILIARY" ) whsp ]
+			#						   ; default structural
+			#	  [ "MUST" oids ]	   ; AttributeTypes
+			#	  [ "MAY" oids ]	   ; AttributeTypes
 			# whsp ")"
-			whsp = /[ ]+/
-			
-			# object descriptors used as schema element names
-			#     qdescrs         = qdescr / ( whsp "(" qdescrlist ")" whsp )
-			#     qdescrlist      = [ qdescr *( qdescr ) ]
-			#     qdescr          = whsp "'" descr "'" whsp
-			qdescr  = %r{ #{whsp} ' '}
-			qdescrs = 
-			
-			
 			LDAP_OBJECTCLASS_DESCRIPTION = %r{
 				\( #{whsp}
-					(#{LDAP_OID})
-					#{whsp}
-					(NAME )
+					(#{numericoid})				# $1
+					#{whsp}                 	
+					(?:NAME (#{qdescrs}))?		# $2
+					#{whsp}						# missing from the rfc's bnf, but necessary
+					(?:DESC (#{qdstring}))? 	# $3
+					#{whsp}                 	
+					(?:(OBSOLETE) )?			# $4
+					#{whsp}                 	
+					(?:SUP (#{oids}))?			# $5
+					(							# $6
+						ABSTRACT
+						|
+						STRUCTURAL
+						|
+						AUXILIARY
+					)?
+					(?:MUST (#{oids}))?			# $7
+					(?:MAY (#{oids}))?			# $8
 				#{whsp} \)
-			}x
+			}ix
 			
 		end # objectClass
 	
--- spec/lib/helpers.rb
+++ spec/lib/helpers.rb
@@ -27,6 +27,23 @@ end
 module Treequel::SpecHelpers
 	include Treequel::TestConstants
 
+	class ArrayLogger
+		### Create a new ArrayLogger that will append content to +array+.
+		def initialize( array )
+			@array = array
+		end
+		
+		### Write the specified +message+ to the array.
+		def write( message )
+			@array << message
+		end
+		
+		### No-op -- this is here just so Logger doesn't complain
+		def close; end
+		
+	end # class ArrayLogger
+
+
 	unless defined?( LEVEL )
 		LEVEL = {
 			:debug => Logger::DEBUG,
@@ -47,7 +64,6 @@ module Treequel::SpecHelpers
 	end
 	
 	
-	
 	### Alter the output of the default log formatter to be pretty in SpecMate output
 	def setup_logging( level=Logger::FATAL )
 
@@ -62,11 +78,15 @@ module Treequel::SpecHelpers
 
 		# Only do this when executing from a spec in TextMate
 		if ENV['HTML_LOGGING'] || (ENV['TM_FILENAME'] && ENV['TM_FILENAME'] =~ /_spec\.rb/)
+			Thread.current['logger-output'] = []
+			logdevice = ArrayLogger.new( Thread.current['logger-output'] )
+			Treequel.logger = Logger.new( logdevice )
+			# Treequel.logger.level = level
 			Treequel.logger.formatter = Treequel::HtmlLogFormatter.new( logger )
 		end
 	end
-	
-	
+
+
 	### Load the test config if it exists and return the specified +section+ of the config 
 	### as a Hash. If the file doesn't exist, or the specified section doesn't exist, an
 	### empty Hash will be returned.
@@ -94,133 +114,6 @@ module Treequel::SpecHelpers
 	
 	
 end
-
-
-# Override the badly-structured output of the RSpec HTML formatter
-require 'spec/runner/formatter/html_formatter'
-
-class Spec::Runner::Formatter::HtmlFormatter
-	def example_failed( example, counter, failure )
-		failure_style = failure.pending_fixed? ? 'pending_fixed' : 'failed'
-		
-		unless @header_red
-			@output.puts "    <script type=\"text/javascript\">makeRed('rspec-header');</script>"
-			@header_red = true
-		end
-		
-		unless @example_group_red
-			css_class = 'example_group_%d' % [current_example_group_number]
-			@output.puts "    <script type=\"text/javascript\">makeRed('#{css_class}');</script>"
-			@example_group_red = true
-		end
-		
-		move_progress()
-		
-		@output.puts "    <dd class=\"spec #{failure_style}\">",
-		             "      <span class=\"failed_spec_name\">#{h(example.description)}</span>",
-		             "      <div class=\"failure\" id=\"failure_#{counter}\">"
-		if failure.exception
-			backtrace = format_backtrace( failure.exception.backtrace )
-			message = failure.exception.message
-			
-			@output.puts "        <div class=\"message\"><code>#{h message}</code></div>",
-			             "        <div class=\"backtrace\"><pre>#{backtrace}</pre></div>"
-		end
-
-		if extra = extra_failure_content( failure )
-			@output.puts( extra )
-		end
-		
-		@output.puts "      </div>",
-		             "    </dd>"
-		@output.flush
-	end
-
-
-	if instance_methods.include?( 'global_styles' )
-		alias_method :default_global_styles, :global_styles
-	else
-		def default_global_styles
-			"/* No default global_styles (methods: %p)?!? */" % [ instance_methods ]
-		end
-	end
-	
-	def global_styles
-		css = default_global_styles()
-		css << %Q{
-			/* Stuff added by #{__FILE__} */
-
-			/* Overrides */
-			#rspec-header {
-				-webkit-box-shadow: #333 0 2px 5px;
-				margin-bottom: 1em;
-			}
-
-			.example_group dt {
-				-webkit-box-shadow: #333 0 2px 3px;
-			}
-
-			/* Style for log output */
-			dd.log-message {
-				background: #eee;
-				padding: 0 2em;
-				margin: 0.2em 1em;
-				border-bottom: 1px dotted #999;
-				border-top: 1px dotted #999;
-				text-indent: -1em;
-			}
-
-			/* Parts of the message */
-			dd.log-message .log-time {
-				font-weight: bold;
-			}
-			dd.log-message .log-time:after {
-				content: ": ";
-			}
-			dd.log-message .log-level {
-				font-variant: small-caps;
-				border: 1px solid #ccc;
-				padding: 1px 2px;
-			}
-			dd.log-message .log-name {
-				font-size: 1.2em;
-				color: #1e51b2;
-			}
-			dd.log-message .log-name:before { content: "«"; }
-			dd.log-message .log-name:after { content:  "»"; }
-
-			dd.log-message .log-message-text {
-				padding-left: 4px;
-				font-family: Monaco, "Andale Mono", "Vera Sans Mono", mono;
-			}
-
-
-			/* Distinguish levels */
-			dd.log-message.debug { color: #666; }
-			dd.log-message.info {}
-
-			dd.log-message.warn,
-			dd.log-message.error {
-				background: #ff9;
-			}
-			dd.log-message.error .log-level,
-			dd.log-message.error .log-message-text {
-				color: #900;
-			}
-			dd.log-message.fatal {
-				background: #900;
-				color: white;
-				font-weight: bold;
-				border: 0;
-			}
-			dd.log-message.fatal .log-name {
-				color:  white;
-			}
-		}
-		
-		return css
-	end
-end # module Treequel::SpecHelpers
 
 
 # vim: set nosta noet ts=4 sw=4:
