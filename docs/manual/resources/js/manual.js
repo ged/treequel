7c35222c5cac8fa6c84fb6725d4beb949c819057
Michael Granger <ged@FaerieMUD.org>
2009-07-17 22:53:50 +0000

    Cleaned up the manual syntax-highlighting a bunch. Red logo!
    --HG--
    extra : convert_revision : svn%3Aa6044dcd-02c3-dd11-8301-000e0c4bc663/trunk%4075

--- docs/manual/resources/js/manual.js
+++ docs/manual/resources/js/manual.js
@@ -3,23 +3,24 @@
 // Auto generate a table of contents if a #auto-toc div exists.
 //
 function generate_toc() {
-	
+
 	$('#auto-toc').append('<ul></ul>');
-		
+
 	$('h2').each( function() {
 		var header = $(this);
 		var html = header.html().replace( /^\s*|\s*$/g, '' );
 		var newid = html.toLowerCase().replace( /\W+/g, '-' );
 		header.prepend( '<a name="' + newid + '" />' );
 
-		console.debug( "Making a link for %s: %o", newid, html );
 		$('#auto-toc ul').append( '<li><a href="#' + newid + '">' + html + "</a></li>" );
 	});
 }
 
 
 function highlight_examples() {
-	SyntaxHighlighter.config.clipboardSwf = 'swf/clipboard.swf';
+	SyntaxHighlighter.defaults['ruler'] = false;
+	SyntaxHighlighter.defaults['toolbar'] = false;
+	SyntaxHighlighter.config.clipboardSwf = null;
 	SyntaxHighlighter.all();
 }
 
