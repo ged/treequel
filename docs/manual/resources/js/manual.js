feb8ef31d0ccb7d5be1b8dbb71b5c31a6ab0880d
Michael Granger <ged@FaerieMUD.org>
2009-07-17 17:59:09 +0000

    Use client-side highlighting instead of Ultraviolet.
    --HG--
    extra : convert_revision : svn%3Aa6044dcd-02c3-dd11-8301-000e0c4bc663/trunk%4074

--- docs/manual/resources/js/manual.js
+++ docs/manual/resources/js/manual.js
@@ -1,25 +1,31 @@
 
+
 // Auto generate a table of contents if a #auto-toc div exists.
 //
 function generate_toc() {
-	if ( $('#auto-toc').length > 0 ) {
-		var toc = $('#auto-toc').wrapInner('<ul></ul>');
-		$('h2').each( function() { append_header_to_toc(toc, this); });
-	}
-};
-
-function append_header_to_toc( toc, item ) {
-	var header = $(item);
-	var html = header.html().replace( /^\s*|\s*$/g, '' );
-	var newid = html.toLowerCase().replace( /\W+/g, '-' );
-	header.prepend( '<a name="' + newid + '" />' );
 	
-	toc.append( '<li><a href="#' + newid + '">' + html + "</a></li>" );
-};
+	$('#auto-toc').append('<ul></ul>');
+		
+	$('h2').each( function() {
+		var header = $(this);
+		var html = header.html().replace( /^\s*|\s*$/g, '' );
+		var newid = html.toLowerCase().replace( /\W+/g, '-' );
+		header.prepend( '<a name="' + newid + '" />' );
 
+		console.debug( "Making a link for %s: %o", newid, html );
+		$('#auto-toc ul').append( '<li><a href="#' + newid + '">' + html + "</a></li>" );
+	});
+}
+
+
+function highlight_examples() {
+	SyntaxHighlighter.config.clipboardSwf = 'swf/clipboard.swf';
+	SyntaxHighlighter.all();
+}
 
 $(document).ready(function() {
 	generate_toc();
+	highlight_examples();
 });
 
 
