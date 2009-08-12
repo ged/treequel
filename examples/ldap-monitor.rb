bd2b69d3a65c8185967be8dddc16832e659bf702
Michael Granger <ged@FaerieMUD.org>
2009-08-11 20:59:52 -0700

    * Added another real-world example and added a bit more work to the web monitor app.  * Made the rev spec tolerant of keywords being turned on or off.

--- examples/ldap-monitor.rb
+++ examples/ldap-monitor.rb
@@ -26,6 +26,32 @@ before do
 		:bind_dn => 'cn=admin,cn=Monitor', :pass => 'monitor' )
 end
 
+helpers do
+
+	### Return a string describing the amount of time in the given number of
+	### seconds in terms a human can understand easily.
+	def time_delta_string( start_time )
+		start = Time.parse( start_time ) or return "some time"
+		seconds = Time.now - start
+
+		return 'less than a minute' if seconds < 60
+
+		if seconds < 50 * 60
+			return "%d minute%s" % [seconds / 60, seconds/60 == 1 ? '' : 's']
+		end
+
+		return 'about an hour'					if seconds < 90 * MINUTES
+		return "%d hours" % [seconds / HOURS]	if seconds < 18 * HOURS
+		return 'one day' 						if seconds <  1 * DAYS
+		return 'about a day' 					if seconds <  2 * DAYS
+		return "%d days" % [seconds / DAYS] 	if seconds <  1 * WEEKS
+		return 'about a week' 					if seconds <  2 * WEEKS
+		return "%d weeks" % [seconds / WEEKS] 	if seconds <  3 * MONTHS
+		return "%d months" % [seconds / MONTHS] if seconds <  2 * YEARS
+		return "%d years" % [seconds / YEARS]
+	end
+
+end
 
 ### GET /
 get '/' do
@@ -41,25 +67,47 @@ end
 # Subsystems
 # 
 
+get '/backends' do
+	subsystem = @monitor.cn( :backends )
+	backends = subsystem.
+		filter( :objectClass => :monitoredObject ).
+		select( :+, :* )
+
+	erb :backends,
+		:locals => {
+			:subsystem => subsystem,
+			:backends => backends,
+		}
+end
+
+
 get '/connections' do
-	connections = @monitor.cn( :connections )
+	subsystem = @monitor.cn( :connections )
+	connections = subsystem.
+		filter( :objectClass => :monitorConnection ).
+		select( :+, :* )
 
 	erb :connections,
 		:locals => {
-			:subsystem => connections,
-			:total => connections.cn( :total ),
-			:current => connections.cn( :current ),
-			:conninfo => connections.children
+			:subsystem => subsystem,
+			:total => subsystem.cn( :total ),
+			:current => subsystem.cn( :current ),
+			:connections => connections.all,
 		}
 end
 
 
 ### Fallback handler for subsystems
 get '/:subsystem' do
-	branch = @monitor.cn( params[:subsystem] )
+	subsystem = @monitor.cn( params[:subsystem] )
+	contents = subsystem.
+		filter( :objectClass => :monitoredObject ).
+		select( :+, :* )
+
 	erb :dump_subsystem,
 		:locals => {
-			:subsystem => branch,
+			:subsystem => subsystem,
+			:contents => contents,
 		}
 end
 
