ba2c87a1d2915c978aa1982caeb89e2c3daacc9b
Mahlon E. Smith <mahlon@martini.nu>
2009-12-07 17:11:43 -0800

    Add scaffolding for Treequel::Control.

---
+++ lib/treequel/control.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/ruby
+
+require 'ldap'
+require 'treequel'
+
+
+### Virtual interface methods for Control modules.
+module Treequel::Control
+
+	### Control API interface method
+	###
+	### If your control is a client control, you should super() to this method
+	### and add your control (in the form of an LDAP::Control object) to the
+	### resulting Array before returning it.
+	def get_client_controls
+		return []
+	end
+
+	### Control API interface method
+	###
+	### If your control is a server control, you should super() to this method
+	### and add your control (in the form of an LDAP::Control object) to the
+	### resulting Array before returning it.
+	def get_server_controls
+		return []
+	end
+
+end # module Treequel::Control
+
+# vim: set nosta noet ts=4 sw=4:
+
+
---
+++ spec/treequel/control_spec.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+BEGIN {
+	require 'pathname'
+	basedir = Pathname.new( __FILE__ ).dirname.parent.parent
+
+	libdir = basedir + "lib"
+
+	$LOAD_PATH.unshift( libdir ) unless $LOAD_PATH.include?( libdir )
+}
+
+require 'spec'
+require 'spec/lib/constants'
+require 'spec/lib/helpers'
+
+require 'treequel'
+require 'treequel/control'
+
+include Treequel::TestConstants
+include Treequel::Constants
+
+#####################################################################
+###	C O N T E X T S
+#####################################################################
+
+describe Treequel, "control" do
+	include Treequel::SpecHelpers
+
+	before( :each ) do
+		@testclass = Class.new { include Treequel::Control }
+		@obj = @testclass.new
+	end
+
+	it "provides a empty client control list by default" do
+		@obj.get_client_controls.should == []
+	end
+
+	it "provides a empty server control list by default" do
+		@obj.get_server_controls.should == []
+	end
+end
+
+# vim: set nosta noet ts=4 sw=4:
